8611decc5bc3a7b4c36b3f32c1ac15d24a1d918f
Unknown <sdethy@gmail.com>
2017-11-06 16:03:19 +0100

    lien css

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+
+	<link rel="stylesheet" href="../Views/css/reset.css">
+	<link rel="stylesheet" href="../Views/css/style.css">
+	
+	<title>Didier Motte</title>
+</head>
+<body>
+	
+	<div id="contenair" class="maincontenair">
+			
+			<div id="card">
+				
+			</div>
+
+
+	</div>
+
+
+</body>
+</html>
+
+
