984179b8e31562a4d9f178c0bd0ad0e1f65f0cfc
Unknown <sdethy@gmail.com>
2017-11-06 16:43:22 +0100

    save css

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<?php  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 
-	<link rel="stylesheet" href="../Views/css/reset.css">
-	<link rel="stylesheet" href="../Views/css/style.css">
+	<link rel="stylesheet" href="Views/css/reset.css">
+	<link rel="stylesheet" href="Views/css/style.css">
 	
 	<title>Didier Motte</title>
 </head>
@@ -14,7 +13,10 @@
 	<div id="contenair" class="maincontenair">
 			
 			<div id="card">
-				
+				<div id="mainlogo">
+				<img src="Views/css/logoMotte.jpg" width="50%" height="50%" alt="logo">
+				<h1 class="maintitle">Denis Motte</h1>
+				</div>
 			</div>
 
 
