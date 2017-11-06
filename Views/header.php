8db4fe718a7da998f91758b4e86e98b29565f990
weichuan.tran <weichuan.tran@gmail.com>
2017-11-06 15:28:57 +0100

    header

---
+++ Views/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>DidierMotte</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+  </head>
+  <body>
+    <nav class="principal" >
+      <div class="navGauche">
+        <ul>
+          <li>Antiquité</li>
+          <li>Décoration</li>
+          <li>Agencement</li>
+        </ul>
+      </div>
+      <div class= "navDroite">
+        <button type="text" name="English">English</button>
+        <button type="text" name="Français">Français</button>
+      </div>
+      <div class="navGaucheBas">
+        <a href="#"><img src="" alt=""></a>
+        <h2>Didier <br/>Motte</h2>
+      </div>
+      <div class="navCentreBas">
+        <ul>
+          <li><a href="#">Présentation</a></li>
+          <li><a href="#">Le stock</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <div class="navDroiteBas">
+        <div class="icon">
+          <i class="fa fa-phone" aria-hidden="true"></i>
+
+        </div>
+        <p>+32 495/20.59.34</p>
+      </div>
