cbee3482fb605568aa67a26db6413b617bf146a2
Slylnss <s.alonsious@web-siouss.be>
2017-11-07 15:20:26 +0100

    creation stock.php et mise a jour presentation.php

--- Views/presentation.php
+++ Views/presentation.php
@@ -1,32 +1,32 @@
 <h1>DIDIER MOTTE</h1>
 
-<img src=" "/>
+<p><img src=" "/></p>
 
-<p><strong>Les meubles ont une histoire, authentique</strong></p>
+<h2>Les meubles ont une histoire, authentique</h2>
 
-<p><h3>Toujours à la recherche de l'extra-ordinaire, de 'linsolite, fasciné par la "petite" histoire du monde, celle des métiers d'autrefois, Didier Motte fait partie de ces quelques connaisseurs, brocanteurs ou décorateurs qui, à travers leur regard et leur toucher, savent percevoir l'histoire des objets anciens pour leur donner une nouvelle légitimité esthétique et fonctionnelle. </h3></p>
+<p>Toujours à la recherche de l'extra-ordinaire, de 'linsolite, fasciné par la "petite" histoire du monde, celle des métiers d'autrefois, Didier Motte fait partie de ces quelques connaisseurs, brocanteurs ou décorateurs qui, à travers leur regard et leur toucher, savent percevoir l'histoire des objets anciens pour leur donner une nouvelle légitimité esthétique et fonctionnelle.</p>
 
-<p><h6> Je fais essentiellement des anciens meubles de commerce, des intérieurs complets de vieux magasins, bistrots, pharmacies... J'ai un entrepôt à Luttre, à Nivelles et à Haerelbeek. Si vous êtes dans le coin, vous pouvez venir jeter un coup d'oeil(appelez-moi auparavant au <a href="www.google.be"> 0495 205 934</a>). A bientôt! - Didier</h6></p>
+<p><em>Je fais essentiellement des anciens meubles de commerce, des intérieurs complets de vieux magasins, bistrots, pharmacies... J'ai un entrepôt à Luttre, à Nivelles et à Haerelbeek. Si vous êtes dans le coin, vous pouvez venir jeter un coup d'oeil(appelez-moi auparavant au <a href="www.google.be"> 0495 205 934</a>). A bientôt! - Didier</em></p>
 
 <h2>Services</h2>
 
-<h3>Location de meubles et accessoires pour tournage.</h3>
+<p>Location de meubles et accessoires pour tournage.</p>
 
 <h2>Livraison</h2>
 
-<h3>Livraison dans toute l'Europe & le monde.</h3>
+<p>Livraison dans toute l'Europe & le monde.</p>
 
 <h2>Ils ont fait confiance</h2>
 
-<a href="http://www.zahia.be/" target="_blank"><button>ZAHIA</button></a>:(Anvers, Gand, Brugge, Hasselt) Bijoux.<br/>
-<a href="http://www.vino-events.com/" target="_blank"><button>VINOPRES</button></a>:(Bruxelles) Publications sur le monde du vin. <br/>
-<a href="http://le-relais-du-triporteur.be/#home" target="_blank"><button>LE RELAIS DU TRIPORTEUR</button></a>:(Bruxelles) Epicerie et restaurant Bio.<br/>
-<a href="http://www.superfillesdutram.com/" target="_blank"><button>LES SUPER FILLES DU TRAM</button></a>:(Bruxelles) Restaurant. <br/>
-<a href="http://www.intaglio.fr/" target="_blank"><button>INTAGLIO</button></a>:(Paris) Imprimeur, graveur, papiers de créations.<br/>
-<a href="http://www.villa-alice.be/" target="_blank"><button>VILLA ALICE</button></a>: (Westande-bad) Restaurant.<br/>
-<h6>MAMMUTI:</h6>(Ile de Ré) Galerie, appartements d'hôtes.<br/>
-<h6>ETABLISSEMENTS MAX BLOCH:</h6>(Bruxelles) Grossiste en tissus.<br/>
-<h6>RAW MATERIALS:</h6>(Amsterdam) Décoration.<br/>
-<h6>MONS 2015:</h6>Restaurant La Maison Folie<br/>
-<h6>LA MERCERIE:</h6> (Ixelles) Salon de thé et Dégustation<br/>
-<h6>My ITALIAN FRIENDS:</h6>(Waterloo) Restaurant et design italien.<br/>
+<p><a title="Zahia, magasins de bijoux et accessoires" href="http://zahia.be" target="_blank">ZAHIA</a>:(Anvers, Gand, Brugge, Hasselt) Bijoux.</p>
+<p><a href="http://www.vino-events.com/" target="_blank">VINOPRES</a>:(Bruxelles) Publications sur le monde du vin. </p>
+<p><a href="http://le-relais-du-triporteur.be/#home" target="_blank">LE RELAIS DU TRIPORTEUR</a>:(Bruxelles) Epicerie et restaurant Bio.</p>
+<p><a href="http://www.superfillesdutram.com/" target="_blank">LES SUPER FILLES DU TRAM</a>:(Bruxelles) Restaurant. </p>
+<p><a href="http://www.intaglio.fr/" target="_blank">INTAGLIO</a>:(Paris) Imprimeur, graveur, papiers de créations.</p>
+<p><a href="http://www.villa-alice.be/" target="_blank">VILLA ALICE</a>: (Westande-bad) Restaurant.</p>
+<p>MAMMUTI:(Ile de Ré) Galerie, appartements d'hôtes.</p>
+<p>ETABLISSEMENTS MAX BLOCH:(Bruxelles) Grossiste en tissus.</p>
+<p>RAW MATERIALS:(Amsterdam) Décoration.</p>
+<p>MONS 2015:Restaurant La Maison Folie</p>
+<p>LA MERCERIE: (Ixelles) Salon de thé et Dégustation</p>
+<p>MY ITALIAN FRIENDS:(Waterloo) Restaurant et design italien.</p>
