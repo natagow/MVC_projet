ccaf30ae30f4d19dfbe5956c7514f3d702aa46d6
Slylnss <s.alonsious@web-siouss.be>
2017-11-07 15:34:23 +0100

    correction fichier Laureen

--- Views/presentation.php
+++ Views/presentation.php
@@ -1,23 +1,13 @@
 <h1>DIDIER MOTTE</h1>
-
 <p><img src=" "/></p>
-
 <h2>Les meubles ont une histoire, authentique</h2>
-
 <p>Toujours à la recherche de l'extra-ordinaire, de 'linsolite, fasciné par la "petite" histoire du monde, celle des métiers d'autrefois, Didier Motte fait partie de ces quelques connaisseurs, brocanteurs ou décorateurs qui, à travers leur regard et leur toucher, savent percevoir l'histoire des objets anciens pour leur donner une nouvelle légitimité esthétique et fonctionnelle.</p>
-
 <p><em>Je fais essentiellement des anciens meubles de commerce, des intérieurs complets de vieux magasins, bistrots, pharmacies... J'ai un entrepôt à Luttre, à Nivelles et à Haerelbeek. Si vous êtes dans le coin, vous pouvez venir jeter un coup d'oeil(appelez-moi auparavant au <a href="www.google.be"> 0495 205 934</a>). A bientôt! - Didier</em></p>
-
 <h2>Services</h2>
-
 <p>Location de meubles et accessoires pour tournage.</p>
-
 <h2>Livraison</h2>
-
 <p>Livraison dans toute l'Europe & le monde.</p>
-
 <h2>Ils ont fait confiance</h2>
-
 <p><a title="Zahia, magasins de bijoux et accessoires" href="http://zahia.be" target="_blank">ZAHIA</a>:(Anvers, Gand, Brugge, Hasselt) Bijoux.</p>
 <p><a href="http://www.vino-events.com/" target="_blank">VINOPRES</a>:(Bruxelles) Publications sur le monde du vin. </p>
 <p><a href="http://le-relais-du-triporteur.be/#home" target="_blank">LE RELAIS DU TRIPORTEUR</a>:(Bruxelles) Epicerie et restaurant Bio.</p>
--- index.php
+++ index.php
@@ -1,29 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-
-	<link rel="stylesheet" href="Views/css/reset.css">
-	<link rel="stylesheet" href="Views/css/style.css">
-	
-	<title>Didier Motte</title>
-</head>
-<body>
-	
-	<div id="contenair" class="maincontenair">
-			
+	<div id="contenair" class="maincontenair">			
 			<div id="card">
 				<div id="mainlogo">
 				<img src="Views/css/logoMotte.jpg" width="50%" height="50%" alt="logo">
-				<h1 class="maintitle">Denis Motte</h1>
+				<h1 class="maintitle">Didier Motte</h1>
 				</div>
 			</div>
-
-
 	</div>
-
-
-</body>
-</html>
 
 
