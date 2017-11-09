b9fc362d338af3cae4527ec23321ac7ec4bd43ba
weichuan.tran <weichuan.tran@gmail.com>
2017-11-09 15:53:42 +0100

    contact css

--- Views/contacts.php
+++ Views/contacts.php
@@ -2,6 +2,8 @@
     <h1> Contact </h1>
     <h2> Le magasin </h2>
     <p> Le magasin est ouvert le samedi matin de 10h00 à 12h30 ou sur rendez-vous. </p>
+  </div>
+
     <h6> Didier motte </h6>
     <input type="button" value="dmotte@skynet.be" href="mailto:dmotte@skynet.be">
     <p> +32 495 20.59.34
